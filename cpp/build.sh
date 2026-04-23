aad2729bbb5ebc628c594dd10c71c8c2f5248c4f
wireacad <wireacad@baesystems.com>
2026-04-23 19:35:16 +0100

    added build to cpp

---
+++ cpp/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+NAME="main.cpp"
+OUT="prog"
+
+RUN=0
+COMPILE=0
+
+# parse flags:
+while getopts "cr" opt; do
+	case $opt in
+		c)
+			COMPILE=1
+			;;
+		r)
+			RUN=1
+			;;
+		*)
+			echo "Usage: $0 [-c] [-r]"
+			exit 1
+			;;
+	esac
+done
+
+##compilation
+if [[ $COMPILE -eq 1 ]]; then
+	g++ "$NAME" -lasound -o "$OUT"
+	echo "compiled"
+fi
+
+if [[ "$RUN" -eq 1 ]]; then
+	echo "running.."
+	arecord -D plughw:0,6 -f S16_LE -c 1 -r 44100 | ./$OUT
+fi
