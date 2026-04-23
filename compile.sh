2301ffd301d85f083aa725dc914a4b1d3d11a81f
wireacad <wireacad@baesystems.com>
2026-04-23 13:44:39 +0100

    adding compile script

---
+++ compile.sh
@@ -0,0 +1,12 @@
+NAME="simple_recording.c"
+OUT="recorder"
+RUN=$1
+
+gcc $NAME -lasound -o $OUT
+
+echo "compiled"
+
+if (${RUN} = run); then
+	echo "running.."
+	arecord -D plughw:0,6 -f S16_LE -c 1 -r 44100 | ./$OUT
+fi
