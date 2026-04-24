0571451c9210297029d07300d0a12854a2f018e7
wireacad <wireacad@baesystems.com>
2026-04-24 19:07:28 +0100

    added Analyzer file

---
+++ cpp/Analyzer.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class Analyzer
+{
+	public:
+		int16_t* LiamFFT(int16_t *buffer)
+		{
+			//plan:
+			//decompose buffer
+			//
+		}
+
+		int16_t* LIamIFFT(int16_t *buffer)
+		{
+
+		}
+	private:
+
+}
